14e6a219298068408a952cea4c5de633ca4652a6
Geo Van O <george.voatalum.rpi.edu>
2014-01-18 14:17:31 -0500

    Moved install functionality inside enableWlan.sh Moved install functionality from the install.sh script to enableWlan.sh . Additionally, removed the need to hard-code the IP in the file, which would cause merge conflicts for users down the road (great catch Elco)

--- utils/enableWlan.sh
+++ utils/enableWlan.sh
@@ -1,27 +1,53 @@
 #!/bin/bash
+### Wifi Checking/Re-Enabling script for Brewpi
+### This checks every 10 minutes to see if the wifi connection is still active via a ping
+### If not, it will re-enable the wlan0 interface
+### Geo Van O, Jan 2014
 ### User-editable settings ###
-# IP of the wifi router (Also known as your gateway IP)
-ROUTER="192.168.1.1"
 # Total number of times to try and contact the router if first packet fails
 MAX_FAILURES=3
 # Time to wait between failed attempts contacting the router
 INTERVAL=15
 ###  DO NOT EDIT BELOW HERE!  ###
+DIR=`pwd`
+
+if [ "$1" = "install" ]; then
+    echo "Installing wifi check/enable script..."
+    gateway=$(/sbin/ip route | awk '/default/ { print $3 }')
+    ### Make sure auto wlan0 is added to /etc/network/interfaces, otherwise it causes trouble bringing the interface back up
+    grep "auto wlan0" /etc/network/interfaces
+    if [ $? -ne 0 ]; then
+        printf '%s\n' 0a "auto wlan0" . w | ed -s /etc/network/interfaces
+    fi
+
+    ### Check if enableWlan is already added to cron file and, if not, add it
+    grep "enableWlan.sh" /etc/cron.d/brewpi
+    if [ $? -eq 0 ]; then
+         echo "Cron entry already exists, skipping..."
+    else
+        logPath=$(grep brewpi.py /etc/cron.d/brewpi|sed -r 's/.*(1>.*)$/\1/')
+        echo "Installing cron job for Wifi checking..."
+        echo "*/10 * * * * $DIR/enableWlan.sh $logPath" >> /etc/cron.d/brewpi
+    fi
+    echo "Wifi check script installed! No further action is needed."
+    echo "You can run ./enablewifi.sh from this directory to manually test if you like."
+    exit 0
+fi
 
 fails=0
 
 while [ $fails -lt $MAX_FAILURES ]; do
 ### Try pinging, and if host is up, exit
-    ping -c 1 -I wlan0 $ROUTER > /dev/null
+    ping -c 1 -I wlan0 $gateway > /dev/null
     if [ $? -eq 0 ]; then
         fails=0
-        echo "Successfully pinged $ROUTER"
+        echo "Successfully pinged $gateway"
         break
     fi
 ### If that didn't work...
     let fails=fails+1
     if [ $fails -lt $MAX_FAILURES ]; then
-        echo "Attempt $fails to reach $ROUTER failed" 1>&2
+        echo "Attempt $fails to reach $gateway failed" 1>&2
         sleep $INTERVAL
     fi
 done
