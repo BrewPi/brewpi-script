85117b464511c2a57e41442fc4eedaac7784f807
Elco Jacobs <github@brewpi.com>
2015-04-15 22:32:13 +0200

    Remove old .pyc (compiled python) files from script directory after updating. They apparently can cause trouble.

--- utils/runAfterUpdate.sh
+++ utils/runAfterUpdate.sh
@@ -7,6 +7,24 @@
 # the script path will one dir above the location of this bash file
 unset CDPATH
 myPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+scriptPath="$myPath/.."
+
+#!/usr/bin/env bash
+
+# Delete old .pyc files and empty directories from script directory
+printf "Cleaning up BrewPi script directory...\n"
+NUM_PYC_FILES=$( find "$scriptPath" -name "*.pyc" | wc -l | tr -d ' ' )
+if [ $NUM_PYC_FILES -gt 0 ]; then
+    find "$scriptPath" -name "*.pyc" -delete
+    printf "Deleted $NUM_PYC_FILES old .pyc files\n"
+fi
+
+NUM_EMPTY_DIRS=$( find "$scriptPath" -type d -empty | wc -l | tr -d ' ' )
+if [ $NUM_EMPTY_DIRS -gt 0 ]; then
+    find "$scriptPath" -type d -empty -delete
+    printf "Deleted $NUM_EMPTY_DIRS empty directories\n"
+fi
+
 
 sudo bash "$myPath"/installDependencies.sh
 sudo bash "$myPath"/updateCron.sh
